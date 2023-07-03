dcee83cb1e3c6549d3e5669c61181799f56878ed
sidali zitouni terki <sidalizitouniterki@MacBook-Air-de-sidali.local>
2023-07-03 16:14:12 +0200

    commentary

--- fonction.h
+++ fonction.h
@@ -1,11 +1,11 @@
 /**
-* @file fonction.h
+*\file fonction.h
 *
-* @author  Sid Ali ZITOUNI TERKI
+*\author  Sid Ali ZITOUNI TERKI
 *
-* @date 21 avril 2023
-* @briefsheder file of the functions' core
-* @see  fonction.cpp
+*\date 21 avril 2023
+*\briefsheder file of the functions' core
+*\see  fonction.cpp
 */
 
 #ifndef fonction_h
@@ -14,12 +14,13 @@
 #include <stdio.h>
 #include <string.h>
 
+
 typedef unsigned int uint;
 
 
 void flush_buffer(void); /*Refresh our buffer */
 
-
-void pendu (char *); /*we search a word playng "Pendu"*/
+/*we search a word playing "hangman game"*/
+void pendu (char *);
 
 #endif /* fonction_h */
--- main.c
+++ main.c
@@ -1,26 +1,35 @@
 /**
-* @file main.c
+* \file main.c
+* \brief Main file of the functions' core
+* \author  Sid Ali ZITOUNI TERKI
+* \version V1
+* \date 21 april 2023
 *
-* @author  Sid Ali ZITOUNI TERKI
-*
-* @date 21 avril 2023
-* @briefs Main file of the functions' core
-* @see  fonction.h
+* \ programme *
 */
 
 #include "fonction.h"
 #include <stdlib.h>
 #include <time.h>
 
+/**
+* \fn int main (void)
+* \brief Program entry.
+*
+* \return EXIT_SUCCESS - Normal program stop.
+* \return EXIT_FAILURE - Program stop with error.
+*/
 
 
 int main(int argc, const char * argv[]) {
 
     srand(time(NULL));
     
+    //We use random for to take a random word from the list.
     uint i = rand() % 11 - 1;
     char* seeks_word[] ={"ALGER", "PROGRAMME", "AVION", "BATEAU", "PAYS", "ECOLE", "RANDOMISER", "EXPLOIT", "AVIATEUR", "TRAVAIL"};
     
+    //We play with the word that was taken randomly.
     pendu(seeks_word[i]);
     
     
